d9ac7a362101116e7b56dffdce77d91c32cd1503
fatima2816 <fatimaabdulwahid5@gmail.com>
2024-03-04 14:46:38 +0500

    changed spare Parts UI

--- third_app/lib/Screen/Spare_Parts/PlotScreen.dart
+++ third_app/lib/Screen/Spare_Parts/PlotScreen.dart
@@ -32,18 +32,113 @@ class _PlotScreenState extends State<PlotScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Flask and Flutter Integration'),
-      ),
-      body: Center(
-        child: plotData.isNotEmpty
-            ? Image.memory(
-                base64.decode(plotData),
-                width: 800,
-                height: 400,
+      body: Stack(
+        children: [
+          Container(
+            height: 720,
+            width: 1370,
+            decoration: BoxDecoration(
+              image: DecorationImage(
                 fit: BoxFit.cover,
-              )
-            : CircularProgressIndicator(),
+                image: AssetImage('assets/background.png'),
+              ),
+            ),
+          ),
+          Positioned(
+            left: 250,
+            top: 70,
+            child: Container(
+              width: 900,
+              height: 600,
+              decoration: BoxDecoration(
+                color: Color(0xFF313134),
+                borderRadius: BorderRadius.circular(20.2151851654),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    margin: EdgeInsets.fromLTRB(50, 15, 0, 25),
+                    child: RichText(
+                      text: TextSpan(
+                        style: TextStyle(
+                          fontFamily: 'Inter',
+                          fontSize: 26.2151851654,
+                          fontWeight: FontWeight.w400,
+                          height: 1.2125,
+                          color: Color(0xFFFFFFFF),
+                        ),
+                        children: [
+                          TextSpan(
+                            text: 'Prediction Preview',
+                            style: TextStyle(
+                              fontFamily: 'Inter',
+                              fontSize: 32,
+                              fontWeight: FontWeight.normal,
+                              height: 1.2125,
+                              color: Color(0xFFFFFFFF),
+                            ),
+                          ),
+                          TextSpan(text: ' '),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Row(
+                    children: [
+                      Container(
+                        // No margin for the second Container
+                        margin: EdgeInsets.fromLTRB(55, 0, 0, 0.10),
+                        width: 500,
+                        height: 8,
+                        decoration: BoxDecoration(
+                          color: Color(0xddff8518),
+                        ),
+                      ),
+                      Container(
+                        // No margin for the second Container
+                        width: 200,
+                        height: 8,
+                        decoration: BoxDecoration(
+                          color: Color(0xddff8518),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 20),
+                  Center(
+                    child: plotData.isNotEmpty
+                        ? Image.memory(
+                            base64.decode(plotData),
+                            width: 800,
+                            height: 400,
+                            fit: BoxFit.cover,
+                          )
+                        : CircularProgressIndicator(),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Positioned(
+            left: 10,
+            top: 10,
+            child: Container(
+              decoration: BoxDecoration(
+                color: Color(0xddff8518), // Replace with your desired color
+                shape: BoxShape.circle, // Makes the container circular
+              ),
+              child: IconButton(
+                icon: Icon(Icons.arrow_back),
+                color: Colors.white, // Icon color
+                onPressed: () {
+                  // Navigate back when the back button is pressed
+                  Navigator.pop(context);
+                },
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
