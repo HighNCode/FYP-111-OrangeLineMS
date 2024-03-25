7a00027bf213ee214f2a9692fb7e6a31ff2f771f
fatima2816 <fatimaabdulwahid5@gmail.com>
2024-03-25 23:00:16 +0500

    created wheel analysis UI

--- third_app/lib/Screen/Wheel Data OCR/wheel_analysis.dart
+++ third_app/lib/Screen/Wheel Data OCR/wheel_analysis.dart
@@ -3,11 +3,6 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:third_app/Screen/mainPage/EngineerDashboard.dart';
 import 'package:third_app/Screen/mainPage/ManagerDashboard.dart';
-// import 'package:third_app/Screen/Wheel_Analysis/Page1.dart';
-// import 'package:third_app/Screen/fault_detection/Page3.dart';
-// import 'package:third_app/Screen/Fault_Data/first.dart';
-// import 'package:third_app/Screen/Wheel Data OCR/ocr.dart';
-// import 'package:third_app/Screen/Spare_Parts/PlotScreen.dart';
 import 'package:third_app/main.dart';
 import 'package:third_app/app_state.dart';
 
@@ -24,17 +19,16 @@ class MyApp extends StatelessWidget {
   }
 }
 
-//import 'app_data.dart';
 class WheelAnalysis extends StatefulWidget {
   const WheelAnalysis({Key? key}) : super(key: key);
 
   @override
-  _WheelAnalysis createState() => _WheelAnalysis();
+  _WheelAnalysisState createState() => _WheelAnalysisState();
 }
 
-class _WheelAnalysis extends State<WheelAnalysis> {
+class _WheelAnalysisState extends State<WheelAnalysis> {
   String selected_train_no = 'OL001';
-  int _selectedMenu = 0; // Track the selected menu item
+  int _selectedMenu = 0;
   List<String> trainNumbers = List.generate(27, (index) {
     int number = index + 1;
     String formattedNumber = 'OL${number.toString().padLeft(3, '0')}';
@@ -53,13 +47,12 @@ class _WheelAnalysis extends State<WheelAnalysis> {
         automaticallyImplyLeading: false,
         title: Row(
           children: [
-            // Add an image here
             Padding(
               padding: const EdgeInsets.only(right: 0.0),
               child: Image.asset(
-                'assets/Logo.png', // Replace with your image asset
-                width: 70, // Adjust the width as needed
-                height: 70, // Adjust the height as needed
+                'assets/Logo.png',
+                width: 70,
+                height: 70,
               ),
             ),
             Column(
@@ -73,13 +66,11 @@ class _WheelAnalysis extends State<WheelAnalysis> {
             ),
             SizedBox(width: 810),
             Icon(Icons.person, color: Colors.white),
-
             PopupMenuButton<String>(
               offset: Offset(0, 40),
               icon: Icon(Icons.arrow_drop_down, color: Colors.white),
               onSelected: (value) {
                 if (value == 'logout') {
-                  // Navigate to the login page
                   Navigator.push(context,
                       MaterialPageRoute(builder: (context) => LoginScreen()));
                 }
@@ -89,8 +80,8 @@ class _WheelAnalysis extends State<WheelAnalysis> {
                   return PopupMenuItem<String>(
                     value: 'logout',
                     child: Container(
-                      child: Text(choice,
-                          style: TextStyle(color: Colors.black)), // Logout text
+                      child:
+                          Text(choice, style: TextStyle(color: Colors.black)),
                     ),
                   );
                 }).toList();
@@ -99,43 +90,9 @@ class _WheelAnalysis extends State<WheelAnalysis> {
           ],
         ),
         iconTheme: IconThemeData(
-          color: Colors.orange, // Set the icon (menu) color to orange
+          color: Colors.orange,
         ),
       ),
-      // drawer: MyDrawer(
-      //   selectedMenuItem: _selectedMenu,
-      //   onMenuItemSelected: (int index) {
-      //     setState(() {
-      //       if (index == 0) {
-      //         Navigator.push(context,
-      //             MaterialPageRoute(builder: (context) => MyDashboard()));
-      //       } else {
-      //         _selectedMenu = index;
-      //         print(_selectedMenu);
-      //         if (index == 1) {
-      //           Navigator.push(
-      //               context, MaterialPageRoute(builder: (context) => Page1()));
-      //         } else if (index == 2) {
-      //           Navigator.push(
-      //               context, MaterialPageRoute(builder: (context) => first()));
-      //         } else if (index == 3) {
-      //           Navigator.push(
-      //               context, MaterialPageRoute(builder: (context) => Page3()));
-      //         } else if (index == 4) {
-      //           Navigator.push(
-      //               context, MaterialPageRoute(builder: (context) => ocr()));
-      //         } else if (index == 5) {
-      //           Navigator.push(context,
-      //               MaterialPageRoute(builder: (context) => PlotScreen()));
-      //         } else {
-      //           // Handle other menu items or scenarios if needed
-      //           print('fail');
-      //         }
-      //       }
-      //     });
-      //     // Navigator.pop(context);
-      //   },
-      // ),
       body: SingleChildScrollView(
         child: Container(
           height: 720,
@@ -159,158 +116,149 @@ class _WheelAnalysis extends State<WheelAnalysis> {
                     color: Color(0xFF313134),
                     borderRadius: BorderRadius.circular(20.2151851654),
                   ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Container(
-                        margin: EdgeInsets.fromLTRB(50, 15, 0, 25),
-                        child: RichText(
-                          text: TextSpan(
-                            style: TextStyle(
-                              fontFamily: 'Inter',
-                              fontSize: 26.2151851654,
-                              fontWeight: FontWeight.w400,
-                              height: 1.2125,
-                              color: Color(0xFFFFFFFF),
+                  child: SingleChildScrollView(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          margin: EdgeInsets.fromLTRB(50, 15, 0, 25),
+                          child: RichText(
+                            text: TextSpan(
+                              style: TextStyle(
+                                fontFamily: 'Inter',
+                                fontSize: 26.2151851654,
+                                fontWeight: FontWeight.w400,
+                                height: 1.2125,
+                                color: Color(0xFFFFFFFF),
+                              ),
+                              children: [
+                                TextSpan(
+                                  text: 'Wheel Analysis',
+                                  style: TextStyle(
+                                    fontFamily: 'Inter',
+                                    fontSize: 35,
+                                    fontWeight: FontWeight.normal,
+                                    height: 1.2125,
+                                    color: Color(0xFFFFFFFF),
+                                  ),
+                                ),
+                                TextSpan(text: ' '),
+                              ],
                             ),
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(left: 35, top: 42),
+                          child: Row(
                             children: [
-                              TextSpan(
-                                text: 'Wheel Analysis',
+                              Text(
+                                'Train Number',
                                 style: TextStyle(
-                                  fontFamily: 'Inter',
-                                  fontSize: 35,
-                                  fontWeight: FontWeight.normal,
-                                  height: 1.2125,
-                                  color: Color(0xFFFFFFFF),
+                                  fontSize: 22,
+                                  color: Color(0xFFF5F5F5),
                                 ),
                               ),
-                              TextSpan(text: ' '),
                             ],
                           ),
                         ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(left: 35, top: 42),
-                        child: Row(
-                          children: [
-                            Text(
-                              'Train Number',
-                              style: TextStyle(
-                                fontSize: 22,
-                                color: Color(0xFFF5F5F5),
-                                // Add other styling properties as needed
-                              ),
+                        Padding(
+                          padding: const EdgeInsets.only(
+                              left: 35, top: 6, bottom: 30),
+                          child: Container(
+                            width: 200,
+                            decoration: BoxDecoration(
+                              color: const Color(0xFF313134),
+                              borderRadius: BorderRadius.circular(10),
                             ),
-                          ],
-                        ),
-                      ),
-                      Padding(
-                        padding:
-                            const EdgeInsets.only(left: 35, top: 6, bottom: 30),
-                        child: Container(
-                          width: 200,
-                          decoration: BoxDecoration(
-                            color: const Color(0xFF313134),
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          child: Align(
-                            alignment: Alignment.centerRight,
-                            child: DropdownButtonFormField<String>(
-                              value: selected_train_no,
-                              items: trainNumbers.map((String value) {
-                                return DropdownMenuItem<String>(
-                                  value: value,
-                                  child: Text(
-                                    value,
-                                    style: const TextStyle(
-                                      color: Color(0xFFF5F5F5),
-                                      fontSize: 16,
+                            child: Align(
+                              alignment: Alignment.centerRight,
+                              child: DropdownButtonFormField<String>(
+                                value: selected_train_no,
+                                items: trainNumbers.map((String value) {
+                                  return DropdownMenuItem<String>(
+                                    value: value,
+                                    child: Text(
+                                      value,
+                                      style: const TextStyle(
+                                        color: Color(0xFFF5F5F5),
+                                        fontSize: 16,
+                                      ),
+                                    ),
+                                  );
+                                }).toList(),
+                                onChanged: (String? newValue) {
+                                  setState(() {
+                                    selected_train_no =
+                                        newValue ?? selected_train_no;
+                                    print(
+                                        "Selected option: $selected_train_no");
+                                    isTrainNumberSelected = true;
+                                    TrainToFlask(selected_train_no);
+                                  });
+                                },
+                                decoration: InputDecoration(
+                                  filled: true,
+                                  fillColor: const Color(0xFF313134),
+                                  border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(10),
+                                    borderSide: BorderSide.none,
+                                  ),
+                                  focusedBorder: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(10),
+                                    borderSide: const BorderSide(
+                                      color: Color(0xFFFF8518),
+                                      width: 2.0,
                                     ),
                                   ),
-                                );
-                              }).toList(),
-                              onChanged: (String? newValue) {
-                                setState(() {
-                                  selected_train_no =
-                                      newValue ?? selected_train_no;
-                                  print("Selected option: $selected_train_no");
-                                  isTrainNumberSelected = true;
-                                  TrainToFlask(selected_train_no);
-                                });
-                              },
-                              decoration: InputDecoration(
-                                filled: true,
-                                fillColor: const Color(0xFF313134),
-                                border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(10),
-                                  borderSide: BorderSide.none,
                                 ),
-                                focusedBorder: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(10),
-                                  borderSide: const BorderSide(
-                                    color: Color(0xFFFF8518),
-                                    width: 2.0,
-                                  ),
+                                elevation: 1,
+                                icon: const Icon(
+                                  Icons.arrow_drop_down,
+                                  color: Color(0xFFF5F5F5),
                                 ),
+                                dropdownColor: const Color(0xFF313134),
                               ),
-                              elevation: 1,
-                              icon: const Icon(
-                                Icons.arrow_drop_down,
-                                color: Color(0xFFF5F5F5),
-                              ),
-                              dropdownColor: const Color(0xFF313134),
                             ),
                           ),
                         ),
-                      ),
-                      if (apiResponse.isNotEmpty)
-                        Container(
-                          padding: const EdgeInsets.only(left: 35),
-                          child: Column(
-                            children: (jsonDecode(apiResponse) as List<dynamic>)
-                                .map((item) {
-                              int wheelSetNumber = item['WheelSetNumber'];
-                              bool isFit = item['Fit'];
+                        if (apiResponse.isNotEmpty)
+                          Center(
+                            child: Container(
+                              padding: const EdgeInsets.only(left: 35),
+                              child: DataTable(
+                                columns: [
+                                  DataColumn(
+                                      label: Text('Wheel Set Number',
+                                          style: TextStyle(
+                                              fontSize: 18,
+                                              color: Colors.white))),
+                                  DataColumn(
+                                      label: Text('Result',
+                                          style: TextStyle(
+                                              fontSize: 18,
+                                              color: Colors.white))),
+                                ],
+                                rows: (jsonDecode(apiResponse) as List<dynamic>)
+                                    .map((item) {
+                                  int wheelSetNumber = item['WheelSetNumber'];
+                                  bool isFit = item['Fit'];
 
-                              return ListTile(
-                                title: Text('Wheel Set Number: $wheelSetNumber',
-                                    style: const TextStyle(
-                                        fontSize: 18, color: Colors.white)),
-                                subtitle: Text(
-                                    'Result: ${isFit ? 'Fit' : 'Unfit'}',
-                                    style: const TextStyle(
-                                        fontSize: 16, color: Colors.white)),
-                              );
-                            }).toList(),
+                                  return DataRow(cells: [
+                                    DataCell(Text('$wheelSetNumber',
+                                        style: TextStyle(
+                                            fontSize: 16,
+                                            color: Colors.white))),
+                                    DataCell(Text('${isFit ? 'Fit' : 'Unfit'}',
+                                        style: TextStyle(
+                                            fontSize: 16,
+                                            color: Colors.white))),
+                                  ]);
+                                }).toList(),
+                              ),
+                            ),
                           ),
-                        ),
-                      // Stack(
-                      //   children: [
-                      //     Positioned(
-                      //       left: 50, // Adjust left position as needed
-                      //       top: 50, // Adjust top position as needed
-                      //       child: Container(
-                      //         height: 50,
-                      //         width: 200,
-                      //         child: ElevatedButton(
-                      //           onPressed: () async {
-                      //             await MyDashboard();
-                      //           },
-                      //           style: ElevatedButton.styleFrom(
-                      //             shape: RoundedRectangleBorder(
-                      //               borderRadius: BorderRadius.circular(30.0),
-                      //             ),
-                      //           ),
-                      //           child: Text(
-                      //             'Submit',
-                      //             style: TextStyle(fontSize: 18),
-                      //           ),
-                      //         ),
-                      //       ),
-                      //     ),
-                      //   ],
-                      // ),
-                    ],
+                      ],
+                    ),
                   ),
                 ),
               ),
@@ -387,6 +335,8 @@ class _WheelAnalysis extends State<WheelAnalysis> {
       // Handle any exceptions
     }
   }
+}
+
 
 // List<Widget> _buildWheelSetInfo() {
 //   // Parse the API response as a list of maps
@@ -412,4 +362,4 @@ class _WheelAnalysis extends State<WheelAnalysis> {
 
 //   return wheelSetInfoWidgets;
 // }
-}
+
